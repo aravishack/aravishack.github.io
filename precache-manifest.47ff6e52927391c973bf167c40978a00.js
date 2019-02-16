cecc81147dd7a8735ba7abc72ec44fc41293ba6e
Aravind S <aravindsklm@gmail.com>
2019-02-16 19:59:44 +0530

    ReactJS Update

---
+++ precache-manifest.47ff6e52927391c973bf167c40978a00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59cda4c250b81d23b680337bb9ad3261",
+    "url": "/static/media/mobilebg.59cda4c2.jpg"
+  },
+  {
+    "revision": "2e96c35be916e8987ce561401442b095",
+    "url": "/static/media/desktop-bg.2e96c35b.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "28abb53fc8beb9fbd993",
+    "url": "/static/js/main.28abb53f.chunk.js"
+  },
+  {
+    "revision": "fda3aaaab390db0a5c8e",
+    "url": "/static/js/2.fda3aaaa.chunk.js"
+  },
+  {
+    "revision": "28abb53fc8beb9fbd993",
+    "url": "/static/css/main.5adf5dfd.chunk.css"
+  },
+  {
+    "revision": "8d2284d2bf48b4feae2c3417e3175fbd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,34 @@
-let cacheName = "myappCache";
-var filesToCache = [
-  "index.html",
-  "js/site.js",
-  "css/style.css",
-  "images/favicon.png",
-  "images/desktop-bg.jpg",
-  "images/mobile-bg-copy.jpg",
-  "images/og-image.jpg"
-];
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-self.addEventListener("install", function(e) {
-  //console.log("[ServiceWorker] Install");
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      //console.log("[ServiceWorker] Caching app shell");
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-self.addEventListener("activate", function(e) {
-  //console.log("[ServiceWorker] Activate");
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(
-        keyList.map(function(key) {
-          if (key !== cacheName) {
-            //console.log("[ServiceWorker] Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  return self.clients.claim();
-});
+importScripts(
+  "/precache-manifest.47ff6e52927391c973bf167c40978a00.js"
+);
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response; // if valid response is found in cache return it
-      } else {
-        return fetch(event.request) //fetch from internet
-          .then(function(res) {
-            return caches.open(cacheName).then(function(cache) {
-              cache.put(event.request.url, res.clone()); //save the response for future
-              return res; // return the fetched data
-            });
-          })
-          .catch(function(err) {
-            // fallback mechanism
-            return caches.open(cacheName).then(function(cache) {
-              return cache.match("/offline.html");
-            });
-          });
-      }
-    })
-  );
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
